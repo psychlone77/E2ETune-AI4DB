5139a97f2ce4c41399f8e321220dbc3044524c5c
psychlone77 <nithikasdias@gmail.com>
2026-05-19 23:25:06 +0530

    Clean up and move files from root folder

--- resetDB.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict, Any
-from Database import reset_db_knobs
-from config import parse_config
-
-
-if __name__ == '__main__':
-    args = parse_config.parse_args("config/config.ini")
-    reset_db_knobs(args)
